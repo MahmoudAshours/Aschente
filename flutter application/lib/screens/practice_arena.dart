29a62b60532ab9f024f5cbc945a8c58ca8365e17
Mahmoud Ashour <mashour365@gmail.com>
2021-05-11 15:46:09 +0200

    Get data from JSON and displayed it

--- flutter application/lib/screens/practice_arena.dart
+++ flutter application/lib/screens/practice_arena.dart
@@ -3,11 +3,19 @@ import 'package:circular_countdown_timer/circular_countdown_timer.dart';
 import 'package:flutter/material.dart';
 
 class PracticeArena extends StatefulWidget {
+  final questions;
+
+  const PracticeArena({Key? key, this.questions}) : super(key: key);
   @override
   _PracticeArenaState createState() => _PracticeArenaState();
 }
 
-class _PracticeArenaState extends State<PracticeArena> {
+class _PracticeArenaState extends State<PracticeArena>
+    with TickerProviderStateMixin {
+  int _questionNumber = 0;
+  final _countDown = CountDownController();
+  int _score = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,15 +39,13 @@ class _PracticeArenaState extends State<PracticeArena> {
               child: Align(
                 alignment: Alignment.topRight,
                 child: CircularCountDownTimer(
-                  duration: 60,
+                  duration: 10,
                   initialDuration: 0,
-                  controller: CountDownController(),
+                  controller: _countDown,
                   width: 100,
                   height: 100,
                   ringColor: Color(0xff160d20),
-                  ringGradient: null,
                   fillColor: Color(0xffcfb34e),
-                  fillGradient: null,
                   backgroundColor: Color(0xff160d20),
                   strokeWidth: 10.0,
                   strokeCap: StrokeCap.round,
@@ -53,7 +59,11 @@ class _PracticeArenaState extends State<PracticeArena> {
                   isTimerTextShown: true,
                   autoStart: true,
                   onComplete: () {
-                    print('Countdown Ended');
+                    setState(() {
+                      if (_questionNumber < 9) {
+                        _getNextQuestion();
+                      }
+                    });
                   },
                 ),
               ),
@@ -62,29 +72,30 @@ class _PracticeArenaState extends State<PracticeArena> {
           Positioned(
             top: MediaQuery.of(context).size.height / 3,
             left: MediaQuery.of(context).size.width / 6 - 24,
+            width: MediaQuery.of(context).size.width / 2,
             child: FadeInUp(
               child: Text(
-                'Q1: What is operating system?',
+                'Q${_questionNumber + 1}: ${widget.questions[_questionNumber]['question']}',
                 style: TextStyle(fontSize: 24, color: Colors.white),
               ),
             ),
           ),
-          for (var i = 0; i < 4; i++)
+          for (var i = 0;
+              i < widget.questions[_questionNumber]['answers'].length;
+              i++)
             Positioned(
               top: MediaQuery.of(context).size.height / 2 + i * 50,
               left: 0,
               child: Container(
                 child: Padding(
                   padding: const EdgeInsets.all(13.0),
-                  child: SlideInLeft(
+                  child: FadeIn(
                     delay: Duration(seconds: i + 1),
                     child: TextButton(
-                      onPressed: () {},
+                      onPressed: () => _checkAnswer(i),
                       child: Text(
-                        '${i + 1}. sad',
-                        style: TextStyle(
-                          fontSize: 28,
-                        ),
+                        '${widget.questions[_questionNumber]['answers'][i]}',
+                        style: TextStyle(fontSize: 28, color: Colors.white),
                       ),
                     ),
                   ),
@@ -94,5 +105,41 @@ class _PracticeArenaState extends State<PracticeArena> {
         ],
       ),
     );
+  }
+
+  _checkAnswer(int? answerIndex) {
+    print(answerIndex);
+    if (answerIndex != null) {
+      if (widget.questions[_questionNumber]['correct_answer'] ==
+          _alphaNumeric(answerIndex + 1)) {
+        _score++;
+      }
+      if (_questionNumber != 9) {
+        _getNextQuestion();
+      } else { 
+      }
+    }
+  }
+
+  _getNextQuestion() {
+    setState(() => _questionNumber++);
+    _countDown.restart();
+  }
+
+  _alphaNumeric(int? index) {
+    switch (index) {
+      case 1:
+        return 'A';
+      case 2:
+        return 'B';
+      case 3:
+        return 'C';
+      case 4:
+        return 'D';
+      case 5:
+        return 'E';
+      case 6:
+        return 'F';
+    }
   }
 }
--- flutter application/lib/screens/questions_preparations.dart
+++ flutter application/lib/screens/questions_preparations.dart
@@ -1,13 +1,13 @@
 import 'dart:convert';
 import 'dart:math';
 
+import 'package:aschente/screens/practice_arena.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 // ignore: must_be_immutable
 class QuestionsPreparations extends StatelessWidget {
   final subject;
-
   List _pickedQuestions = [];
   QuestionsPreparations({Key? key, @required this.subject}) : super(key: key);
 
@@ -16,17 +16,57 @@ class QuestionsPreparations extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: [
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.bottomRight,
+                colors: [
+                  Color(0xff2a1434),
+                  Color(0xff160d20),
+                  Color(0xff142332)
+                ],
+              ),
+            ),
+          ),
           FutureBuilder(
             future: DefaultAssetBundle.of(context)
                 .loadString('assets/$subject.json'),
-            builder: (context, snapshot) {
-              List _questions = json.decode(snapshot.data.toString());
-              for (var i = 0; i < 10; i++) {
-                final random = Random().nextInt(_questions.length - 1);
-                _pickedQuestions.add(_questions[random]);
+            builder: (BuildContext context, snapshot) {
+              if (snapshot.data != null ||
+                  snapshot.connectionState == ConnectionState.done) {
+                List _questions = json.decode(snapshot.data.toString());
+                for (var i = 0; i < 10; i++) {
+                  final random = Random().nextInt(_questions.length);
+                  _pickedQuestions.add(_questions[random]);
+                }
+                Future.microtask(
+                  () => Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(
+                      builder: (_) =>
+                          PracticeArena(questions: _pickedQuestions),
+                    ),
+                  ),
+                );
               }
-              return Container(
-                child: SpinKitCircle(color: Colors.pink),
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      child: SpinKitCircle(color: Colors.pink),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(18.0),
+                      child: Container(
+                        child: Text(
+                          'Preparing your questions',
+                          style: TextStyle(color: Colors.white, fontSize: 25),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               );
             },
           ),
